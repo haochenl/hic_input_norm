8b531ce4e1b99c43e936bfc0476ab7bc8831e40f
haochenl <haochenl@usc.edu>
2016-03-04 01:35:30 -0800

    Add dbscan peak calling method

--- utils.py
+++ utils.py
@@ -6,8 +6,9 @@ import sys
 import os
 import pysam
 from time import time
+import genome
 
-class peak(object):
+class callPeaks(object):
 	"""
 	the related input peak calling algorithms. input filename should be sam or bam file.
 	"""
@@ -17,7 +18,8 @@ class peak(object):
 		file = pysam.AlignmentFile(filename, type)
 		chrs_forward = {}
 		chrs_reverse = {}
-		for ref in file.references:
+		self.references = genome.reSites._chrSort(file.references)
+		for ref in self.references:
 			chrs_forward[ref] = []
 			chrs_reverse[ref] = []
 		itr = file.fetch(until_eof=True)
@@ -40,8 +42,74 @@ class peak(object):
 		self.reads_forward = chrs_forward
 		self.reads_reverse = chrs_reverse
 
-	def dbscan(self, eps, min_samples, output_name):
-		pass
+	def writeBed(self, eps, min_samples):
+		out_forward = open(os.path.splitext(self.filename)[0]+'.forward.bed', 'w')
+		out_reverse = open(os.path.splitext(self.filename)[0]+'.reverse.bed', 'w')
+		for ref in self.references:
+			forwards = self.reads_forward[ref]
+			reverses = self.reads_reverse[ref]
+			clusters_forward = self.dbscan(sorted_list=forwards, eps, min_samples)['clusters']
+			clusters_reverse = self.dbscan(sorted_list=reverses, eps, min_samples)['clusters']
+			for i in clusters_forward:
+				coords = [forwards[elem] for elem in i] 
+				out_forward.write('\t'.join([ref] + [str(min(coords))] + [str(max(coords))]) + '\n')
+			for i in clusters_reverse:
+				coords = [reverses[elem] for elem in i] 
+				out_reverse.write('\t'.join([ref] + [str(min(coords))] + [str(max(coords))]) + '\n')
+		out_forward.close()
+		out_reverse.close()
+
+	def dbscan(self, sorted_list, eps, min_samples):
+		clusters = []
+		visited = [0] * len(sorted_list)
+		noise = [0] * len(sorted_list)
+		for i in range(len(sorted_list)):
+			if visited[i] == 1:
+				pass
+			else:
+				visited[i] = 1
+				neighbors = self._regionQuery(sorted_list, index=i, eps)
+				if len(neighbors) < min_samples:
+					noise[i] = 1
+				else:
+					C = self._expandCluster(sorted_list, index=i, visited, neighbors, eps, min_samples, clusters)
+					clusters.append(C)
+		return {'clusters': clusters, 'noises': noise}
+
+	def _expandCluster(self, sorted_list, index, visited, neighbors, eps, min_samples, clusters):
+		C = [index]
+		for i in neighbors:
+			if visited[i] == 0:
+				visited[i] = 1
+				neighbors_prime = self._regionQuery(sorted_list, index=i, eps)
+				if len(neighbors_prime) >= min_samples:
+					new_neighbors = [elem for elem in neighbors_prime if elem not in neighbors]
+					neighbors += new_neighbors
+			if len(clusters) == 0:
+				C.append(i)
+			else:
+				if i not in clusters[-1]:
+					C.append(i)
+		return C
+
+	def _regionQuery(self, sorted_list, index, eps):
+		points = [index]
+		plus = index + 1
+		minus = index - 1
+		while plus <= len(sorted_list):
+			if sorted_list[plus] - sorted_list[index] <= eps:
+				points.append(plus)
+				plus += 1
+			else:
+				break
+		while minus >= 0:
+			if sorted_list[index] - sorted_list[minus] <= eps:
+				points.append(minus)
+				minus -= 1
+			else:
+				break
+		return points
+
 
 
 
